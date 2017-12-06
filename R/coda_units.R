9745caa253846d11bc0f3fd07d8e2277aebfa682
Justin Silverman <jsilve24@gmail.com>
2017-12-06 16:43:37 -0500

    Added Function for Evidence Information 2 log fold change

---
+++ R/coda_units.R
@@ -0,0 +1,17 @@
+
+#' Calculate Equilvalent Log Fold Change for Given Change in Evidence Information
+#'
+#' @param x Evidence Information Units
+#' @param r Number of parts in numerator of log-ratio
+#' @param s number of parts in denominator of log-ratio
+#'
+#' @details EI is defined on log-ratio scale and for a given balance can be
+#' derived as \deqn{y = sqrt(rs/(r+s))log(Gm(X^+)/Gm(X^-))}
+#' @return
+#' @export
+#'
+#' @examples
+#' eu2logfoldchange(1)
+eu2logfoldchange <- function(x, r=1, s=1){
+  exp(x/(sqrt(r*s/(r+s))))
+}

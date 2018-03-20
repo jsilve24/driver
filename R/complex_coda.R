403f747120f9be67b2b8327bdd5e3182ca5fa3d2
Justin Silverman <jsilve24@gmail.com>
2018-03-20 17:18:31 -0400

    added phi calculations
    Signed-off-by: Justin Silverman <jsilve24@gmail.com>

--- R/complex_coda.R
+++ R/complex_coda.R
@@ -1,6 +1,8 @@
 #' Convert CoDA covariance matricies between representations
 #'
 #' \code{ilrvar}, \code{clrvar}, and \code{varmat} (variation matrix).
+#' \code{ilrvar2phi} calculates phi statistics (for proportionality)
+#' from an ILR covariance matrix as described in Lovell (2015).
 #'
 #' @param Sigma covariance matrix in specified transformed space
 #' @param V ILR contrast matrix (i.e., transformation matrix of ILR)
@@ -19,7 +21,22 @@
 #' clrvar2ilrvar(Sigma.clr, V)
 #' clrvar2varmat(Sigma.clr)
 #' ilrvar2varmat(Sigma, V)
+#' ilrvar2phi(Sigma,V)
 NULL
+
+#' @rdname convert_coda_covariance
+#' @export
+ilrvar2phi <- function(Sigma, V){
+  Sigma.clr <- ilrvar2clrvar(Sigma,V)
+  phi <- Sigma.clr
+  phi[] <- 0
+  for(i in 1:dim(Sigma.clr)[1]){
+    for (j in 1:dim(Sigma.clr)[2]){
+      phi[i,j] <- Sigma.clr[i,i] + Sigma.clr[j,j] - 2*Sigma.clr[i,j]
+    }
+  }
+  return(phi)
+}
 
 
 #' @rdname convert_coda_covariance
@@ -55,4 +72,5 @@ ilrvar2varmat <- function(Sigma, V){
   Sigma <- ilrvar2clrvar(Sigma, V)
   clrvar2varmat(Sigma)
 }
+
 

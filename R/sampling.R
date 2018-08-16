1a7094fa2d9c89849397ccc90178567e753bc330
Justin Silverman <jsilve24@gmail.com>
2018-08-16 15:03:56 -0400

    Added examples for uniform sampling

--- R/sampling.R
+++ R/sampling.R
@@ -1,9 +1,21 @@
 #' Sample Uniformily from various objects centered at the origin
 #'
+#' @param n number of samples to produce
 #' @param D dimension of object
-#' @param n_samples number of samples to produce
-#' @return matrix of dimension D x n_samples
+#' @return matrix of dimension D x n
 #' @name sample_uniform
+#' @examples
+#' # Sample Uniformily from Sphere (Full)
+#' x <- rUnifSphere(1000, 2, 2)
+#' plot(t(x), asp=1)
+#'
+#' # Sample Uniformily from Sphere (Shell Only)
+#' x <- rUnifSphere(1000, 2, 2, shell_only=TRUE)
+#' plot(t(x), asp=1)
+#'
+#' # Sample Uniformily from Simplex
+#' x <- rUnifSimplex(1000, 2)
+#' plot(t(x), asp=1)
 NULL
 
 #' @rdname sample_uniform
@@ -11,20 +23,20 @@ NULL
 #' @param radius radius of sphere to sample from
 #' @param shell_only if TRUE samples from shell of object only, if false
 #'   samples from entire region of object
-rUnifSphere <- function(D, radius, n_samples=1, shell_only=FALSE){
-  z <- matrix(rnorm(D*n_samples), D, n_samples)
+rUnifSphere <- function(n, D, radius=1, shell_only=FALSE){
+  z <- matrix(rnorm(D*n), D, n)
   sf <- sqrt(colSums(z^2))
   if (shell_only) {
     ru <- radius
   } else {
-    ru <- radius*runif(n_samples)^(1/D)
+    ru <- radius*runif(n)^(1/D)
   }
   sweep(z, 2, ru/sf, FUN="*")
 }
 
 #' @rdname sample_uniform
 #' @export
-rUnifSimplex <- function(D,  n_samples=1){
-  z <- matrix(rexp(D*n_samples), n_samples, D)
+rUnifSimplex <- function(n, D){
+  z <- matrix(rexp(D*n), n, D)
   t(miniclo(z))
 }
